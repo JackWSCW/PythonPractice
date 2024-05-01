bc9904222cd78c7255c4d1ead909b29283b2c004
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-05-02 10:28:26 +1200

    first (v1)

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello")
+# ask the user their name 
+input("What is your name?")
+# greet the user and introduce the quiz
+print("Welcome to this quiz!")
+print("This quiz is about Mt Everest.")
+# ask the user a question
+input("How tall is Mount Everest in meters?")
+# tell them the correct answer
+print("The correct answer is 8849 meters!")
+# end the quiz
+print("That is the end of the quiz, thank you for playing!")
