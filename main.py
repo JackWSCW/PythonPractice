5ab6b6db421ae4a6c690917a0b09cab6feaa2a59
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-05-07 14:00:20 +1200

    Now

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 print("Hello")
 # ask the user their name 
-input("What is your name?")
-# greet the user and introduce the quiz
-print("Welcome to this quiz!")
-print("This quiz is about Mt Everest.")
+name = input("What is your name?")
+# greet the user and ask their name
+print("Welcome to this quiz,", name)
+print("This quiz is about fish.")
 # ask the user a question
-input("How tall is Mount Everest in meters?")
-# tell them the correct answer
-print("The correct answer is 8849 meters!")
-# end the quiz
+answer = input("What is the best estimate for how many fish are in the whole ocean?")
+if answer == "3,500,000,000,000":
+    print("Correct!")
+# check the users answer and give them feedback
+print("The answer is 3,500,000,000,000!")
+# ask the user a question
+answer = input("What is the worlds largest fish?")
+if answer == "The Whale Shark":
+    print("Correct!")
+# check the users answer and give them feedback
+print("The answer is the Whale Shark!")
+# end the quiz and thank them for playing
 print("That is the end of the quiz, thank you for playing!")
