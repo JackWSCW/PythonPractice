a8e646732498e3b257581b0674832eb433cb6bf6
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-05-23 10:28:39 +1200

    end pt 5

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 play = "yes"
+import random
 
 QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
-import random
 GOOD_COMMENTS = ["Way to go!", "Keep it up!", "Fantastic!"]
 BAD_COMMENTS = ["Keep Trying", "Maybe next time", "Don't give up"]
+QUESTIONS = ["What is the best estimate for how many fish are in the whole ocean?",
+             "What is the name of the largest fish in the ocean?",
+             "What is the average lifespan of a shark?"]
+OPTIONS = [["4,200,000,000,000", "2,560,000,000", "3,500,000,000,000", "9,150,000,500,000"],
+           ["The whale shark", "The basking shark", "Tiger shark", "Great white shark",]
+           ["10-20 years", "20-30 years", "30-40 years", "40-30 years"]]
+SHORT_OPTIONS = ["a", "b", "c", "d"]
+ANSWERS = [3,1,2]
 # ask the user their name 
 name = input("What is your name?")
 
@@ -23,58 +31,34 @@ while True:
 # Start the quiz
 while play == "yes":
     score = 0
-    question_attempts = tries
-    while question_attempts > 0:
-        # ask the user a question
-        question = "What is the best estimate for how many fish are in the whole ocean?"
-        a = "4,200,000,000,000"
-        b = "2,560,000,000"
-        c = "3,500,000,000,000"
-        d = "9,150,000,500,000"
-        answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
-        if answer == c or answer == "c".lower():
-            print("Correct!")
-            score += 5
-            print(random.choice(GOOD_COMMENTS))
-            break
-        elif answer == "":
-            print("Not Sure?")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != d and answer != "d":
-            print("That wasn't an option")
-        else:
-            print("You are wrong")
-            print(random.choice(BAD_COMMENTS))
+    # loop through each questions/answers
+    for i in range(len(QUESTIONS)):
+        question_attempts = tries
+        while question_attempts > 0:
+            # ask the user a question
+            answer = input(QUESTION_FORMAT.format(QUESTIONS[i], OPTIONS[i][0],
+                                                OPTIONS[i][1], OPTIONS[i][2], OPTIONS[i][3])).lower()
+    
+            # check the users answer
+            if answer == OPTIONS[i][ANSWERS[i]] or answer == SHORT_OPTIONS[ANSWERS[i]]:
+                print("Correct!")
+                score += 5
+                print(random.choice(GOOD_COMMENTS))
+                break
+            elif answer == "":
+                print("Not sure?")
+            elif answer in SHORT_OPTIONS or answer in OPTIONS[i]:
+                print("Wrong!")
+                print(random.choice(BAD_COMMENTS))
+            else:
+                print("That wasn't an option")
+
 
             question_attempts -= 1
-        print("The answer is 3,500,000,000")
-    # check the users answer and give them feedback
-    print("The answer is 3,500,000,000,000!")
-    
-    # ask the user a question
-    question_attempts = tries
-    while question_attempts > 0:
-        question = "What is the worlds largest fish?"   
-        a = "The Whale Shark"
-        b = "The Blue Whale"
-        c = "The Basking Shark"
-        d = "Megalodon"     
-        answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()        
-        if answer == a or answer == "a".lower():
-            print("Correct!")
-            print(random.choice(GOOD_COMMENTS))
-            score += 5
-            break
-        elif answer == "":
-            print("Not Sure?")
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != d and answer != "d":
-            print("That wasn't an option")
-        else:
-            print("You are wrong")
-            print(random.choice(BAD_COMMENTS))
-        question_attempts -= 1
-    # check the users answer and give them feedback
-    print("The answer is the Whale Shark!")
-  
+        print("The answer is 3,500,000,000,000")
+
+        
+
     # end the quiz and thank them for playing
     print("That is the end of the quiz {}. Thank you for playing! Your final score was {}".format(name, score))
 
